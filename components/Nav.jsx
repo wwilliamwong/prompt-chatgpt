814ad092a731376f0b75a280a6b68ab286d7439a
William Wong <wonghinfung0325@gmail.com>
2023-05-23 07:56:56 -0400

    feat: added signout button and profile image

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,6 +25,20 @@ const Nav = () => {
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
           </div>
         ) : (
           <></>
