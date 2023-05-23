f139a833d67d81d46dcbba19cb7143785f22b58f
William Wong <wonghinfung0325@gmail.com>
2023-05-23 10:34:29 -0400

    feat: added more links and signout button in the dropdown

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -39,7 +39,7 @@ const Nav = () => {
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
-              Create Post
+              Create Prompt
             </Link>
 
             <button type="button" onClick={signOut} className="outline_btn">
@@ -93,6 +93,23 @@ const Nav = () => {
                 >
                   My Profile
                 </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
               </div>
             )}
           </div>
