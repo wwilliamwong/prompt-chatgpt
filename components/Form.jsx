92dc37add49bf0e18d615c026fde3a6800720c27
William Wong <wonghinfung0325@gmail.com>
2023-05-27 19:19:29 -0400

    feat: create createprompt page

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import Link from "next/link";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform.
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {` `}
+            <span>(#product, #webdevelopment, #idea)</span>
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          ></textarea>
+        </label>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
